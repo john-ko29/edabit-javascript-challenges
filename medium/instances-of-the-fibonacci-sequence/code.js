b08ea8f42f8252e904723136bd691c6e8e78a77c
John Ko <jko121790@gmail.com>
2020-10-06 00:18:35 -0700

    fibonacci complete

--- medium/instances-of-the-fibonacci-sequence/code.js
+++ medium/instances-of-the-fibonacci-sequence/code.js
@@ -1,5 +1,21 @@
 function fibSeq(end) {
-  // Your code here.
+  const sequence = [];
+  if (end  === 0) {
+    return sequence;
+  }
+  if (end === undefined) {
+    return undefined;
+  }
+  for (let i = 0; i < end; i++) {
+    if (i === 0) {
+      sequence.push(0);
+    } else if (i === 1) {
+      sequence.push(1);
+    } else {
+      sequence.push(sequence[i-1] + sequence[i-2]);
+    }
+  }
+  return sequence;
 }
 
 module.exports = fibSeq;
