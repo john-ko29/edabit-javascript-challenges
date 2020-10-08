8c13dda323168156febd97dbb425072d254e94f4
John Ko <jko121790@gmail.com>
2020-10-08 02:19:28 -0700

    is making progress done

--- medium/is-johnny-making-progress/code.js
+++ medium/is-johnny-making-progress/code.js
@@ -1,5 +1,16 @@
 function progressDays(runs) {
-  // Your code here.
+  let progress = 0;
+  let current = null;
+  for (let i = 0; i < runs.length; i++) {
+    if (current === null) {
+      current = runs[i]
+    }
+    if (current < runs[i]) {
+      progress++;
+    }
+    current = runs[i];
+  }
+  return progress;
 }
 
 module.exports = progressDays;
