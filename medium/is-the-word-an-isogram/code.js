7d2d01cf6472b040f8d0e57b7a64bdfb500d5b31
John Ko <jko121790@gmail.com>
2020-10-11 00:00:35 -0700

    isogram complete

--- medium/is-the-word-an-isogram/code.js
+++ medium/is-the-word-an-isogram/code.js
@@ -1,5 +1,14 @@
 function isIsogram(str) {
+  const word = str.toLowerCase();
   const obj = {}
+  for (let i = 0; i < word.length; i++) {
+    if  (obj[word[i]] === 1) {
+      return false;
+    } else {
+      obj[word[i]] = 1;
+    }
+  }
+  return true;
 }
 
 module.exports = isIsogram;
