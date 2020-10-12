f207a7f355c1a3cbe48ad056216e0da8bb296235
John Ko <jko121790@gmail.com>
2020-10-12 00:07:33 -0700

    maskify complete

--- medium/maskify-the-string/code.js
+++ medium/maskify-the-string/code.js
@@ -1,5 +1,13 @@
 function maskify(str) {
-  // Your code here.
+  const newStr = str.split("");
+  if (str.length < 5) {
+    return str;
+  }
+  for (let i = 0; i < str.length - 4; i++) {
+    newStr[i] = "#";
+  }
+  const returnStr = newStr.join("")
+  return  returnStr;
 }
 
 module.exports = maskify;
