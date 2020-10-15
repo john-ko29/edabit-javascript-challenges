28f54c4806fa4b0ad1174f9388c4853e206fcc9c
John Ko <jko121790@gmail.com>
2020-10-15 00:11:59 -0700

    attempted wave

--- medium/mexican-wave-simulator/code.js
+++ medium/mexican-wave-simulator/code.js
@@ -1,5 +1,17 @@
 function wave(str) {
-  // Your code here.
+  const test = str.trim();
+  const arr = [];
+  if (test.length === 0) {
+    return arr;
+  }
+  for (let i = 0; i < str.length; i++) {
+    let wave = str.toLowerCase();
+    if (str[i] !== " ") {
+      wave[i] = wave[i].toUpperCase();
+      arr.push(wave)
+    }
+  }
+  return arr;
 }
 
 module.exports = wave;
