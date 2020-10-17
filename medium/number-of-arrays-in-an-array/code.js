40f24c3fd8c3f1709f747415023e750cc08e1e5d
John Ko <jko121790@gmail.com>
2020-10-17 00:07:22 -0700

    number of arrays complete

--- medium/number-of-arrays-in-an-array/code.js
+++ medium/number-of-arrays-in-an-array/code.js
@@ -1,5 +1,11 @@
 function numOfSubbarrays(arr) {
-  // Your code here.
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > 1) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = numOfSubbarrays;
