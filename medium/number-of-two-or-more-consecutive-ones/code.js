1f2ca4b338ce71bb130872a5a82bc196fe2046d0
John Ko <jko121790@gmail.com>
2020-10-18 01:10:27 -0700

    two or more consectives done

--- medium/number-of-two-or-more-consecutive-ones/code.js
+++ medium/number-of-two-or-more-consecutive-ones/code.js
@@ -1,5 +1,19 @@
 function countOnes(arr) {
-  // Your code here.
+  let count = 0;
+  let inARow = 0;
+  let current = null;
+  for (let i = 0; i < arr.length; i++) {
+    if (current !== arr[i]) {
+      current = arr[i];
+      inARow = 1;
+    } else if (current === arr[i] && arr[i] === 1) {
+      inARow++;
+      if (inARow === 2) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 
 module.exports = countOnes;
