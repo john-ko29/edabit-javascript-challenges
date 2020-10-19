be8e5fcb3dbf3062857db0b5cbec1db1168a6dc9
John Ko <jko121790@gmail.com>
2020-10-19 00:13:33 -0700

    attempted seesaw

--- medium/numeric-seesaw/code.js
+++ medium/numeric-seesaw/code.js
@@ -1,5 +1,32 @@
+const { parse } = require("@babel/core");
+
 function seesaw(num) {
-  // Your code here.
+  let numL = num + "";
+  let numLength = null;
+  if (numL.length % 2 === 0) {
+    numLength = numL.length;
+  } else {
+    numLength = numL.length - 1;
+  }
+  const numString = numLength + "";
+  let left = "";
+  let right = "";
+  for (let i = 0; i < numLength / 2; i++) {
+    left += numString[i]
+  }
+  for (let i = numL.length - 1; i > numLength / 2; i--) {
+    right += numString[i];
+  }
+  left = parseInt(left);
+  right = parseInt(right);
+  console.log(left, right);
+  if (left > right) {
+    return "left";
+  } else  if (left < right) {
+    return "right";
+  } else {
+    return "balanced";
+  }
 }
 
 module.exports = seesaw;
