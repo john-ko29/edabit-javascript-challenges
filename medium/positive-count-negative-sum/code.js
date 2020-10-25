6c09cadc0686323026760ff999e4c8f774e70aa4
John Ko <jko121790@gmail.com>
2020-10-25 01:10:20 -0700

    positive count done

--- medium/positive-count-negative-sum/code.js
+++ medium/positive-count-negative-sum/code.js
@@ -1,5 +1,19 @@
 function countPosSumNeg(arr) {
-  // Your code here.
+  const newArr = [];
+  let positiveCount = 0;
+  let sumNegative = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      positiveCount++;
+    } else {
+      sumNegative += arr[i];
+    }
+  }
+  if (arr.length !== 0) {
+  newArr.push(positiveCount);
+  newArr.push(sumNegative);
+  }
+  return newArr;
 }
 
 module.exports = countPosSumNeg;
