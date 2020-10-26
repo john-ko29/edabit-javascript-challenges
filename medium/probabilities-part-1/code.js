10b7ffd196a552623a8438eacbbdbea7b11bb764
John Ko <jko121790@gmail.com>
2020-10-26 09:05:56 -0700

    probability part 1 complete

--- medium/probabilities-part-1/code.js
+++ medium/probabilities-part-1/code.js
@@ -1,5 +1,12 @@
 function probability(arr, num) {
-  // Your code here.
+  let success = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (num <= arr[i]) {
+      success++
+    }
+  }
+  const probability = (success / arr.length) * 100;
+  return (parseFloat(probability.toFixed(1)));
 }
 
 module.exports = probability;
