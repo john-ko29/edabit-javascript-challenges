53acfeb226f52eb8090392c481b318642af5f642
John Ko <jko121790@gmail.com>
2020-10-27 03:45:20 -0700

    product of all numbers

--- medium/product-of-all-other-numbers/code.js
+++ medium/product-of-all-other-numbers/code.js
@@ -1,5 +1,15 @@
 function getProducts(arr) {
-  // Your code here.
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let product = 1;
+    for (let k = 0; k < arr.length; k++) {
+      if (i !== k)  {
+        product *= arr[k];
+      }
+    }
+    newArr.push(product);
+  }
+  return newArr;
 }
 
 module.exports = getProducts;
