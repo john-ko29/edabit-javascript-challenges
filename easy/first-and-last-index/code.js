e533547fcfd036aca3470238a76dee78ba995905
John Ko <jko121790@gmail.com>
2020-11-09 00:35:17 -0800

    first and last index complete

--- easy/first-and-last-index/code.js
+++ easy/first-and-last-index/code.js
@@ -1,5 +1,17 @@
 function charIndex(word, char) {
-  // Your code here.
+  const arr = [];
+  let last = word.indexOf(char);
+  if (last === -1) {
+    return undefined;
+  }
+  arr.push(last);
+  for (let i = last; i < word.length; i++) {
+    if (word[i] === char) {
+      last = i;
+    }
+  }
+  arr.push(last);
+  return arr;
 }
 
 module.exports = charIndex;
