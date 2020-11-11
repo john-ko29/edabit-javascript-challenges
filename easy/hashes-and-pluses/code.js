972ebb83d61c5e6d939d463a8e62ab07dee5b6c2
John Ko <jko121790@gmail.com>
2020-11-11 00:02:18 -0800

    hashes and plus complete

--- easy/hashes-and-pluses/code.js
+++ easy/hashes-and-pluses/code.js
@@ -1,5 +1,19 @@
 function hashPlusCount(str) {
-  // Your code here.
+  const arr = [];
+  let hash = 0;
+  let plus = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "#") {
+      hash++;
+    }
+    if (str[i] === "+") {
+      plus++;
+    }
+  }
+  arr.push(hash);
+  arr.push(plus);
+
+  return  arr;
 }
 
 module.exports = hashPlusCount;
