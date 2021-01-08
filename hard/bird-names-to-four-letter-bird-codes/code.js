37f53214618b84de1224dbc4dc933cdae879cafb
John Ko <jko121790@gmail.com>
2021-01-07 22:07:26 -0800

    bird name

--- hard/bird-names-to-four-letter-bird-codes/code.js
+++ hard/bird-names-to-four-letter-bird-codes/code.js
@@ -2,6 +2,10 @@ function birdCode(arr) {
   let name = "";
   const words = arr.split(" ");
   const nameLength = words.length;
+
+  if (words.length === 1) {
+
+  }
 }
 
 module.exports = birdCode;
