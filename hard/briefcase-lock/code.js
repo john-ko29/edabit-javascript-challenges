406c85bc48eb7d9d74876b05b78537a977637d2e
John Ko <jko121790@gmail.com>
2021-02-03 00:42:21 -0800

    briefcase lock attempt

--- hard/briefcase-lock/code.js
+++ hard/briefcase-lock/code.js
@@ -1,5 +1,30 @@
 function minTurns(current, target) {
-  // Your code here.
+  let total = 0;
+  for (let i  = 0; i  < current.length; i++) {
+    let currentNumber = parseInt(current[i])
+    let targetNumber = parseInt(target[i])
+    let forwardTurn = 0;
+    let backwardTurn = 0;
+    for (let k = currentNumber; k !== targetNumber; k++) {
+      if (k === 10) {
+        k -= 10;
+      }
+      forwardTurn++;
+    }
+    for (let k = currentNumber; k !== targetNumber; k--)  {
+      if (k === 0) {
+        k += 10;
+      }
+      backwardTurn++;
+    }
+    if (forwardTurn > backwardTurn) {
+      total += backwardTurn;
+    } else {
+      total += forwardTurn;
+    }
+  }
+
+  return total;
 }
 
 module.exports = minTurns;
