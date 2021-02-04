f82d12476236e8c241bb57ae8e7fe6ce1e450d2c
John Ko <jko121790@gmail.com>
2021-02-04 00:02:51 -0800

    broken bridge attemp

--- hard/broken-bridge/code.js
+++ hard/broken-bridge/code.js
@@ -1,5 +1,23 @@
 function canPatch(bridge, planks) {
-  // Your code here.
+  let holeLength = 0;
+  for (let i = 0; i <bridge.length; i++) {
+    if (bridge[i]  ===  0) {
+      holeLength++;
+    } else {
+      if (holeLength === 0 || holeLength === 1) {
+        holeLength = 0;
+      } else  {
+        for (let k = 0; k < planks.length; k++)  {
+          if ((planks[i] + 1) === holeLength) {
+            holeLength = 0;
+            planks.splice(k, 1)
+            break;
+          }
+          return false;
+        }
+      }
+    }
+  }
 }
 
 module.exports = canPatch;
