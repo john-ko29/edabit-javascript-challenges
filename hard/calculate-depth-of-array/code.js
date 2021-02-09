1a43b843e286d70c73153ba43047244cf49dedcf
John Ko <jko121790@gmail.com>
2021-02-08 23:27:23 -0800

    calculate depth arry

--- hard/calculate-depth-of-array/code.js
+++ hard/calculate-depth-of-array/code.js
@@ -1,5 +1,12 @@
 function depth(arr) {
-  // Your code here.
+  let depthTotal = 1;
+  for (let i =  0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      depthTotal += depth(arr[i])
+    }
+  }
+
+  return depthTotal;
 }
 
 module.exports = depth;
