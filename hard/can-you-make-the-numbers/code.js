99853f5f2109b298914b8e8af2b49234c1494a39
John Ko <jko121790@gmail.com>
2021-02-10 00:29:47 -0800

    can you build

--- hard/can-you-make-the-numbers/code.js
+++ hard/can-you-make-the-numbers/code.js
@@ -1,5 +1,10 @@
 function canBuild(digits, arr) {
-  // Your code here.
+  const numDigits = [];
+  for (let i = 0; i < digits.length; i++) {
+    for (let k = 0; k < digits[i]; k++) {
+      numDigits.push(i)
+    }
+  }
 }
 
 module.exports = canBuild;
