614a9fd6fd514cfe56d060d7cea8bea6f6c42ef6
John Ko <jko121790@gmail.com>
2021-02-15 07:29:06 -0800

    checkerboard generator

--- hard/checkerboard-generator/code.js
+++ hard/checkerboard-generator/code.js
@@ -1,5 +1,24 @@
 function checkerBoard(n, el1, el2) {
-  // Your code here.
+  if (el1 === el2) {
+    return "invalid";
+  }
+  const board = [];
+  const pattern = [];
+  pattern.push(el1);
+  pattern.push(el2);
+  for (let i = 0; i < n; i++) {
+    let currentPattern = 0;
+    const row = [];
+    if (i % 2 === 1)  {
+      currentPattern = 1;
+    }
+    for (let k = 0; k < n; k++) {
+      row.push(pattern[currentPattern]);
+      currentPattern = (currentPattern + 1) % 2;
+    }
+    board.push(row);
+  }
+  return board;
 }
 
 module.exports = checkerBoard;
