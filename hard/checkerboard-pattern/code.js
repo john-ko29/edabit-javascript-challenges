6f2d36250a31fb8f0c4ee242f05d1cf9a526d57f
John Ko <jko121790@gmail.com>
2021-02-15 22:41:50 -0800

    checkerboard pattern

--- hard/checkerboard-pattern/code.js
+++ hard/checkerboard-pattern/code.js
@@ -1,5 +1,20 @@
 function isCheckerboard(arr) {
-  // Your code here.
+  let previous = arr[0][0];
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < arr[i].length; k++) {
+      if (i === 0 && k === 0) {
+        continue;
+      }
+      if (arr.length === 2 && arr[0][1] === arr[1][0] && arr[0][0] === arr[1][1] && i === 1)  {
+        continue;
+      }
+      if (previous === arr[i][k]) {
+        return false;
+      }
+      previous = arr[i][k];
+    }
+  }
+  return true;
 }
 
 module.exports = isCheckerboard;
