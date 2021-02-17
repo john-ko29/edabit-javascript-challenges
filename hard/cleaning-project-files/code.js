a6b3e700183d08b6bdcc964f4fff7513fc56a114
John Ko <jko121790@gmail.com>
2021-02-16 23:59:06 -0800

    cleaning project

--- hard/cleaning-project-files/code.js
+++ hard/cleaning-project-files/code.js
@@ -1,5 +1,12 @@
 function cleanUp(files, sort) {
-  // Your code here.
+  const sortedFiles = [];
+  for (let i = 0; i < files.length; i++) {
+    let dotLocation = files[i].indexOf(".");
+    const fileName = files[i].substring(0, dotLocation);
+    console.log(dotLocation, fileName);
+
+  }
+  console.log(sortedFiles);
 }
 
 module.exports = cleanUp;
