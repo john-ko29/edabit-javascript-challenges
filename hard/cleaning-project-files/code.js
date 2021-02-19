5bbd1bf56b39df5d59623206006e467aeb76d637
John Ko <jko121790@gmail.com>
2021-02-19 00:37:12 -0800

    cleaning project

--- hard/cleaning-project-files/code.js
+++ hard/cleaning-project-files/code.js
@@ -5,14 +5,22 @@ function cleanUp(files, sort) {
     const fileName = files[i].substring(0, dotLocation);
     console.log(dotLocation, fileName);
     if (i === 0) {
-      sortedFiles.push(files[i])
+      const firstFile = [files[0]]
+      sortedFiles.push(firstFile)
+      continue;
     }
 
+    let moveOn = false;
     for (let k = 0; k < sortedFiles.length; k++) {
-      if (sortedFiles[k][0] === fileName) {
+      if (sortedFiles[k][0].substring(0, dotLocation) === fileName) {
         sortedFiles[k].push(files[i]);
+        moveOn = true;
       }
     }
+    if (moveOn) {
+      continue;
+    }
+    sortedFiles.push([files[i]]);
   }
   console.log(sortedFiles);
 
