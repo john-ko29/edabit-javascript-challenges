d007b2b3950d3f506b0583fc35da249d5cb17e80
John Ko <jko121790@gmail.com>
2021-02-21 07:34:16 -0800

    combiniator

--- hard/combinator/code.js
+++ hard/combinator/code.js
@@ -1,5 +1,22 @@
 function combinator(arr) {
-  // Your code here.
+  const newArr = [];
+  if (!arr.length) {
+    return newArr;
+  }
+  for (let i = 0; i < arr[0].length; i++) {
+    const tempArr = [];
+    for (let k = 0; k < arr[0].length; k++) {
+      let word = "";
+      if (arr[0][i].length === 1) {
+        word = "" + arr[0][i] + arr[1][k];
+      } else {
+        word = arr[0][i] + " " + arr[1][k];
+      }
+      tempArr.push(word);
+    }
+    newArr.push(tempArr);
+  }
+  return newArr;
 }
 
 module.exports = combinator;
