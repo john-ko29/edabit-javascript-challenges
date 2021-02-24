6dd7b42713f1bb572ecbd16b62492830e3bb3cab
John Ko <jko121790@gmail.com>
2021-02-24 01:41:40 -0800

    compass

--- hard/compass-directions/code.js
+++ hard/compass-directions/code.js
@@ -1,5 +1,20 @@
 function finalDirection(initial, turns) {
-  // Your code here.
+  const directions = ["N", "E", "S", "W"];
+  let currentDirection = directions.indexOf(initial);
+
+  for (let i = 0; i < turns.length; i++) {
+    if (turns[i] === "L") {
+      currentDirection -= 1;
+    } else   {
+      currentDirection += 1;
+    }
+  }
+
+  if (currentDirection < 0) {
+    currentDirection += 4;
+  }
+
+  return directions[currentDirection % 4];
 }
 
 module.exports = finalDirection;
