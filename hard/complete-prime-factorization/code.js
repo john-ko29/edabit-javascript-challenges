8c4dd1d85afca84327687a84971951e87b043efd
John Ko <jko121790@gmail.com>
2021-02-24 23:54:08 -0800

    prime factor

--- hard/complete-prime-factorization/code.js
+++ hard/complete-prime-factorization/code.js
@@ -1,5 +1,15 @@
 function completeFactorization(num) {
-  // Your code here.
+  const factors = [];
+
+  for (let i = 2; i <= num; i++) {
+    if (num % i == 0) {
+      factors.push(i);
+      num = num / i;
+      i--;
+    }
+  }
+
+  return factors;
 }
 
 module.exports = completeFactorization;
