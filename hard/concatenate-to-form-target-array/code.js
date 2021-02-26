2e5921dad47093bed94ba75e1af040e9b7d15567
John Ko <jko121790@gmail.com>
2021-02-25 22:52:27 -0800

    concatenate to array

--- hard/concatenate-to-form-target-array/code.js
+++ hard/concatenate-to-form-target-array/code.js
@@ -1,5 +1,18 @@
 function canConcatenate(arr, target) {
-  // Your code here.
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < arr[i].length; k++) {
+      if (target.includes(arr[i][k])) {
+        const removeIndex = target.indexOf(arr[i][k]);
+        target.splice(removeIndex, 1)
+      } else {
+        return false;
+      }
+    }
+  }
+  if (target.length !== 0) {
+    return false;
+  }
+  return  true;
 }
 
 module.exports = canConcatenate;
