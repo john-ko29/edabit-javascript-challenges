03b6e926c30ad8649ac82def9a30368598de255f
John Ko <jko121790@gmail.com>
2021-02-26 23:10:55 -0800

    concert seats

--- hard/concert-seats/code.js
+++ hard/concert-seats/code.js
@@ -1,5 +1,16 @@
 function canSeeStage(seats) {
-  // Your code here.
+  const rowLength = seats[0].length;
+  for (let i = 0; i < rowLength; i++)  {
+    let min = seats[0][i];
+    for (let k = 1; k < seats.length; k++) {
+      if (min >= seats[k][i]) {
+        return false;
+      } else {
+        min = seats[k][i]
+      }
+    }
+  }
+  return true;
 }
 
 module.exports = canSeeStage;
