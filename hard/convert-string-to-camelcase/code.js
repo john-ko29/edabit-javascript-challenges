75fe486fccd092c63bd25e450ddb0062f45a874a
John Ko <jko121790@gmail.com>
2021-03-01 23:47:08 -0800

    convert string to camel

--- hard/convert-string-to-camelcase/code.js
+++ hard/convert-string-to-camelcase/code.js
@@ -1,5 +1,21 @@
 function toCamelCase(str) {
-  // Your code here.
+  let connect = null;
+  if (str.includes("-")) {
+    connect = "-";
+  } else if (str.includes("_")) {
+    connect = "_";
+  } else {
+    return str;
+  }
+
+  const newStr = str.split(connect);
+
+  for (let i  = 1; i < newStr.length; i++) {
+    newStr[i].replace(newStr[i].charAt(0), newStr[i].charAt(0).toUpperCase())
+  }
+
+  console.log(newStr)
+  return newStr.join("")
 }
 
 module.exports = toCamelCase;
