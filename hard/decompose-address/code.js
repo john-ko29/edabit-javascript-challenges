d03eb0ac54de740eaaef19c6cce98c6a6332ef6d
John Ko <jko121790@gmail.com>
2021-03-04 00:03:14 -0800

    decompose address

--- hard/decompose-address/code.js
+++ hard/decompose-address/code.js
@@ -1,5 +1,18 @@
 function decomposeAddress(str) {
-  // Your code here.
+  const address = str.split(" ");
+  if (address[2].length === 2)  {
+    address[1] += " "  + address[2];
+    address.splice(2, 1);
+  }
+
+  if (address[3].length !== 2) {
+    const secondWord = address[3].substr(0, address[3].length - 1);
+    address[2] +=  " " + secondWord;
+    address.splice(3, 1);
+  } else {
+    address[2] = address[2].substr(0, address[2].length - 1);
+  }
+  return address;
 }
 
 module.exports = decomposeAddress;
