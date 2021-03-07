ec18ef75d6fe38c37cce1297a32c2790a481b046
John Ko <jko121790@gmail.com>
2021-03-07 07:30:10 -0800

    digits battle

--- hard/digits-battle/code.js
+++ hard/digits-battle/code.js
@@ -1,5 +1,30 @@
 function battleOutcome(num) {
-  // Your code here.
+  let firstDigit = null;
+  let secondDigit = null;
+  let finalNumber = "";
+
+  const numString = "" + num;
+
+  for (let i = 0; i < numString.length; i++) {
+    if (i % 2  === 0) {
+      firstDigit = parseInt(numString[i]);
+    } else {
+      secondDigit = parseInt(numString[i])
+      if (firstDigit > secondDigit) {
+        finalNumber += firstDigit;
+      } else if (secondDigit > firstDigit)  {
+        finalNumber += secondDigit;
+      }
+      firstDigit = null;
+      secondDigit = null;
+    }
+  }
+
+  if (firstDigit !== null && secondDigit === null) {
+    finalNumber += firstDigit;
+  }
+
+  return parseInt(finalNumber);
 }
 
 module.exports = battleOutcome;
