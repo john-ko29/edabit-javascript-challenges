d7ae63578ea36cc3bb09404f6bff1d223976e201
John Ko <jko121790@gmail.com>
2021-03-10 00:06:08 -0800

    bigrams

--- hard/do-all-bigrams-exist/code.js
+++ hard/do-all-bigrams-exist/code.js
@@ -1,10 +1,12 @@
 function canFind(bigrams, words) {
   for (let i = 0; i < bigrams.length; i++) {
+    let isBigram = false;
     for (let k = 0; k < words.length; k++)  {
-      if(!words[k].includes(bigrams[i]))  {
-        return false;
+      if(words[k].includes(bigrams[i]))  {
+        isBigram = true;
       }
     }
+    if(!isBigram) return false;
   }
   return true;
 }
