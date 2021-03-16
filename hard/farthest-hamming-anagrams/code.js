70db601ecfe20d6a7011c78aa945c93221920a9c
John Ko <jko121790@gmail.com>
2021-03-16 00:28:44 -0700

    anagrams

--- hard/farthest-hamming-anagrams/code.js
+++ hard/farthest-hamming-anagrams/code.js
@@ -3,15 +3,13 @@ function maxHam(s1, s2) {
   const str2 = s2.split("");
   let counter = 0;
 
-  console.log(str1, str2);
-
   for (let i = 0; i < s1.length; i++) {
     console.log(s1[i], s2[i], str2);
     if (s1[i] === s2[i] && str2.includes(s1[i])) {
-      counter++;
       const removeIndex = str2.indexOf(s1[i]);
       str2.splice(removeIndex, 1);
     } else if (str2.includes(s1[i]))  {
+      counter++;
       const removeIndex = str2.indexOf(s1[i]);
       str2.splice(removeIndex, 1);
     } else {
@@ -19,13 +17,11 @@ function maxHam(s1, s2) {
     }
   }
 
-  console.log(str1, str2);
-
   if (str2.length > 0) {
     return false;
   }
 
-  if (counter === 0) {
+  if (counter === s1.length) {
     return true;
   } else {
     return counter;
