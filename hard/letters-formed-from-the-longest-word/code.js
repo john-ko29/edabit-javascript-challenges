6fe0074d2d7ed7bc34886cdbd0482d0ef1252c72
John Ko <jko121790@gmail.com>
2021-04-29 00:09:31 -0700

    letters found

--- hard/letters-formed-from-the-longest-word/code.js
+++ hard/letters-formed-from-the-longest-word/code.js
@@ -1,5 +1,28 @@
 function canForm(arr) {
-  // Your code here.
+  let bigIndex = 0;
+  let longestWordLength = arr[0].length;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (longestWordLength < arr[i].length)  {
+      longestWordLength = arr[i].length;
+      bigIndex = i;
+    }
+  }
+
+  const longestWord = arr[bigIndex];
+
+  arr.splice(bigIndex, 1);
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < arr[i].length; k++) {
+      if (!longestWord.includes(arr[i][k])) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 module.exports = canForm;
