67c56d727fbc480005433f14a952cad881ab8861
John Ko <jko121790@gmail.com>
2021-05-06 08:15:31 -0700

    wave

--- hard/mexican-wave-simulator/code.js
+++ hard/mexican-wave-simulator/code.js
@@ -1,5 +1,16 @@
 function wave(str) {
-  // Your code here.
+  const wave = [];
+  const strWave = str.split("")
+  for (let i = 0; i < str.length; i++)  {
+    if (strWave[i] === " ") {
+      continue;
+    } else {
+      strWave[i] =  strWave[i].toUpperCase();
+    }
+    wave.push(strWave)
+  }
+
+  return wave.join("");
 }
 
 module.exports = wave;
