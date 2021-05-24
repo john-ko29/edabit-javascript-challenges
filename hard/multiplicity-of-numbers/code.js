4592c9f429b060b9e5511e16c29c7ad7d128e4f2
John Ko <jko121790@gmail.com>
2021-05-24 00:00:47 -0700

    multiplicity

--- hard/multiplicity-of-numbers/code.js
+++ hard/multiplicity-of-numbers/code.js
@@ -1,5 +1,16 @@
 function multiplicity(arr) {
-  // Your code here.
+  const obj = {};
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (obj.hasOwnProperty(arr[i])) {
+      obj[arr[i]] += 1;
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+
+  return newArr;
 }
 
 module.exports = multiplicity;
